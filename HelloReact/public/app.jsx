d3d6e5f5f9216807ba59c8eb91111404d43ccd9b
MIchael <michael.olson756@gmail.com>
2016-09-06 17:15:49 -0600

    dynamically adding text to DOM

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,4 +1,26 @@
+var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return{
+      name: 'React',
+      message: 'Default Message'
+    };
+  },
+  render: function (){
+    var name = this.props.name;
+    var message = this.props.message
+    return (
+      <div>
+        <h1>Hello {name}</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = "Mike"
+
 ReactDOM.render(
-  <h1>HELLO React</h1>,
+  <Greeter name={firstName} message="message from prop"/>,
+
   document.getElementById('app')
 );
